b51b7b36a84d1eecb06d2d161405c6b281e4e07e
James Hughes <hugjimbo77@gmail.com>
2017-10-17 18:14:37 +0100

    displaying board to user

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,6 @@
 class Board
+
+  def grid
+    return ("0 0\n0 0")
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,4 +4,9 @@ describe Board do
     board = Board.new
     expect(board).to be_instance_of(Board)
   end
+
+  it 'should be create a 2x2 board' do
+    board = Board.new
+    expect(board.grid).to eq("0 0\n0 0")
+  end
 end
